75f62b101d6df620f0c493e952ab7f65d2a4c779
glibk <glib.kozub@gmail.com>
2018-04-17 11:14:06 +0300

    Add nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -14,7 +14,7 @@ class Blog extends Component {
           <nav>
             <ul>
               <li><NavLink
-                to="/"
+                to="/posts/"
                 exact
                 activeClassName="my-active"
                 activeStyle={{
@@ -29,10 +29,10 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact component={Posts}/>
         <Switch>
           <Route path="/new-post" component={NewPost}/>
-          <Route path="/:id" exact component={FullPost}/>
+          <Route path="/posts" component={Posts}/>
+          <Redirect from="/" to="/posts"/>
         </Switch>
       </div>
     );
